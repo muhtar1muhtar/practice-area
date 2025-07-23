c9816c4ec9af4f849f71e3efae1108508f836ff9
muhtar1muhtar <muhtar1muhtar@gmail.com>
2025-07-24 00:05:34 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+arr = np.array([567396, 567889, 567445, 568034])
+print(arr)
---
+++ test.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# my_list = [19, 'female', 27.9, 0, True, 'southwest', 16884.924]
+# print(my_list)
+
+# print(
+#     f"* The index 0 is:  {my_list[0]}, and its data type is {type(my_list[0])}"
+# )
+# print(
+#     f"* The index 1 is:  {my_list[1]}, and its data type is {type(my_list[1])}"
+# )
+
+# arr = np.array(my_list)
+# print(arr)
+
+# print(arr.dtype)
+
+# arr = np.array(my_list, dtype=object)
+# print(arr)
+
+# print(arr.dtype)
+
+my_list = [8, 8.5]
+print(my_list)
+
+arr = np.array(my_list, dtype=np.float64)
+print(arr)
